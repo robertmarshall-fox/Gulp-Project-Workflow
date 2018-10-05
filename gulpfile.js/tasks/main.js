97eca1ef23c57045b826e6cb0c9c2d01ef175740
Rob Marshall <rob@fox.agency>
2018-10-05 15:53:42 +0100

    Updated main Gulp file

--- gulpfile.js/tasks/main.js
+++ gulpfile.js/tasks/main.js
@@ -4,8 +4,4 @@ var gulp = require('gulp');
 gulp.task('default', ['general']);
 
 // Build without launch
-gulp.task('general', ['images', 'js', 'css', 'browser-sync']);
-
-gulp.task('stage', ['general', 'dist'], function(){
-    gulp.start('deploy::staging');
-});
+gulp.task('general', ['js', 'css', 'browser-sync']);
