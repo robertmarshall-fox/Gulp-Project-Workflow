d28aaa91823d46034841e13f14575c05793e4869
Rob Marshall <rob@fox.agency>
2018-10-08 16:24:20 +0100

    Added automated wp-cli install to enviroments

--- gulpfile.js/tasks/server/setup.js
+++ gulpfile.js/tasks/server/setup.js
@@ -1,16 +1,55 @@
 var gulp                = require('gulp');
-var download            = require("gulp-download");
+var shell               = require('gulp-shell');
 
-var exec                = require('child_process').exec;
+var GulpSSH             = require('gulp-ssh');
+
+var config              = require('../../../workflow-config');
 
 /**
  * 1. setup local wp-cli
  */
 
+// Download WP-CLI to local environment
+
+gulp.task('local-wp-cli-setup', () => {
+    console.log('Add WP-CLI to local environment'.red)
+    return gulp.src('*.js', {read: false})
+    .pipe(shell([
+        'cd ../../../ && curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
+    ]))
+});
 
 
- gulp.task( 'local-wp-cli-setup', function(cb) {
-    console.log('Add WP-CLI to local dev'.red);
-    download('https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar')
-     .pipe(gulp.dest('../../../'));
- });
+// Download WP-CLI to stage environment
+gulp.task( 'stage-wp-cli-setup', function(cb) {
+
+    // Create object for stage
+    var stageSSH = new GulpSSH({
+        ignoreErrors: false,
+        sshConfig: config.staging.sslConfig
+    });
+
+    console.log('Add WP-CLI to stage environment'.red);
+    // Move into the public dir and download wp-cli
+    return stageSSH
+      .shell('cd public_html && curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar', {filePath: 'shell.log'})
+      .pipe(gulp.dest('./'));
+});
+
+
+
+// Download WP-CLI to live environment
+gulp.task( 'live-wp-cli-setup', function(cb) {
+
+    // Create object for stage
+    var liveSSH = new GulpSSH({
+        ignoreErrors: false,
+        sshConfig: config.live.sslConfig
+    });
+
+    console.log('Add WP-CLI to live environment'.red);
+    // Move into the public dir and download wp-cli
+    return liveSSH
+      .shell('cd public_html && curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar', {filePath: 'shell.log'})
+      .pipe(gulp.dest('./'));
+});
