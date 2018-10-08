de5f9eecbdc2575905f5512fd250da0fc0b1cd25
Rob Marshall <rob@fox.agency>
2018-10-08 09:27:21 +0100

    Project is no longer based off this, sorry Dan

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,7 +1,2 @@
-// ==== GULPFILE ==== //
-
-// This configuration follows the modular design of the `gulp-starter` project by Dan Tello: https://github.com/greypants/gulp-starter
-// Explore `tasks` for more...
-
 var requireDir = require('require-dir');
 requireDir('./tasks');
