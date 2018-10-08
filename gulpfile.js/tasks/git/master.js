717c75c23d3292b0606dbc55f7aafb9127856fb8
Rob Marshall <rob@fox.agency>
2018-10-08 11:29:39 +0100

    Added staging and master tasks

---
+++ gulpfile.js/tasks/git/master.js
@@ -0,0 +1,102 @@
+var gulp                = require('gulp');
+var fs                  = require('fs');
+var fileExists          = require('file-exists');
+var git                 = require('gulp-git');
+var prompt              = require('gulp-prompt');
+
+var runSequence         = require('run-sequence');
+
+var config              = require('../../../gulpconfig');
+
+/**
+ * git-master   -   Push files to master branch
+ * *****************************************************************************
+ */
+
+/**
+ * These tasks do the following to stage the project:
+ * 1. Confirm ready to push to master branch
+ * 2. Move branch to master
+ * 3. Get commit message
+ * 4. Merge built branch to master
+ * 5. Publishes Git
+ * 6. Moves current branch to build
+ */
+
+/**
+ * TODO
+ * Both master and stage use almost the same functions
+ * Turn these to one page, that can be passed a master/stage argument?
+ */
+
+ gulp.task( 'git-master', function() {
+         console.log('Lets push to master branch');
+         runSequence(
+             'git-confirm-master-merge',
+             'git-merge-build',
+             'git-commit-staging-merge',
+             'git-publish-stage',
+             'git-checkout-build'
+         );
+ });
+
+
+ gulp.task('git-confirm-staging-merge', function( done ){
+     return gulp.src( './*')
+     .pipe(prompt.prompt({
+         type: 'confirm',
+         name: 'start',
+         message: 'Are you sure you everything is error free? Remeber, this goes LIVE'
+     }, function(res){
+         if(res.start){
+             gulp.start('git-move-to-master')
+         }
+     }));
+ });
+
+
+// Move to stage branch
+gulp.task( 'git-move-to-master', function( done ) {
+    console.log('Moved to master branch');
+    git.checkout('master', function (err) {
+        if (err) throw err;
+    });
+});
+
+
+// Merge stage to current branch
+gulp.task('git-merge-stage', function(){
+    git.merge('stage', function (err) {
+        if (err) {
+            done(err);
+        }
+        done()
+    });
+});
+
+
+// Ask user what commit message
+// Commit files
+gulp.task('git-commit-master-merge', function( done ){
+    return gulp.src( './*')
+    .pipe( prompt.prompt({
+        type: 'input',
+        name: 'task',
+        message: 'What commit message would you like to add?'
+    }, function(res){
+        console.log('Committing merge');
+        git.commit(() => {
+            res.task, {args: '-a'}
+            done()
+        });
+    }));
+});
+
+
+// Push all current files to master
+gulp.task('git-publish-master', function(){
+    console.log('Publish to master');
+    git.push('origin', 'master', function (err) {
+        if (err) throw err;
+    });
+});
---
+++ gulpfile.js/tasks/git/stage.js
@@ -0,0 +1,96 @@
+var gulp                = require('gulp');
+var fs                  = require('fs');
+var fileExists          = require('file-exists');
+var git                 = require('gulp-git');
+var prompt              = require('gulp-prompt');
+
+var runSequence         = require('run-sequence');
+
+var config              = require('../../../gulpconfig');
+
+/**
+ * git-stage   -   Push files to staging branch
+ * *****************************************************************************
+ */
+
+/**
+ * These tasks do the following to stage the project:
+ * 1. Confirm ready to push to stage branch
+ * 2. Move branch to stage
+ * 3. Get commit message
+ * 4. Merge built branch to stage
+ * 5. Publishes Git
+ * 6. Moves current branch to build
+ */
+
+ gulp.task( 'git-stage', function() {
+         console.log('Lets push to staging branch');
+         runSequence(
+             'git-confirm-staging-merge',
+             'git-merge-build',
+             'git-commit-staging-merge',
+             'git-publish-stage',
+             'git-checkout-build'
+         );
+ });
+
+
+ gulp.task('git-confirm-staging-merge', function( done ){
+     return gulp.src( './*')
+     .pipe(prompt.prompt({
+         type: 'confirm',
+         name: 'start',
+         message: 'Are you sure you are ready to stage?'
+     }, function(res){
+         if(res.start){
+             gulp.start('git-move-to-stage')
+         }
+     }));
+ });
+
+
+// Move to stage branch
+gulp.task( 'git-move-to-stage', function( done ) {
+    console.log('Moved to stage branch');
+    git.checkout('stage', function (err) {
+        if (err) throw err;
+    });
+});
+
+
+// Merge build to current branch
+gulp.task('git-merge-build', function(){
+    git.merge('build', function (err) {
+        if (err) {
+            done(err);
+        }
+        done()
+    });
+});
+
+
+// Ask user what commit message
+// Commit files
+gulp.task('git-commit-staging-merge', function( done ){
+    return gulp.src( './*')
+    .pipe( prompt.prompt({
+        type: 'input',
+        name: 'task',
+        message: 'What commit message would you like to add?'
+    }, function(res){
+        console.log('Committing merge');
+        git.commit(() => {
+            res.task, {args: '-a'}
+            done()
+        });
+    }));
+});
+
+
+// Push all current files to master
+gulp.task('git-publish-stage', function(){
+    console.log('Publish to stage');
+    git.push('origin', 'stage', function (err) {
+        if (err) throw err;
+    });
+});
