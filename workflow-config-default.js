7ee9d9251f63e1478cbfae0850bde02659a3c614
Rob Marshall <rob@fox.agency>
2018-10-08 17:24:56 +0100

    Default settings

--- workflow-config-default.js
+++ workflow-config-default.js
@@ -1,9 +1,30 @@
-module.exports = {
-    sslConfig: {
-        host: '',
-        port: 22,
-        username: '',
-        privateKey: fs.readFileSync('/Users/#####/.ssh/id_rsa')
-    }
+var gulp         = require('gulp'); // Gulp of-course
+var fs           = require('fs');
 
+const computerName = '';
+
+module.exports = {
+    themeFolder: '',
+    local: {
+        computerName: computerName,
+    },
+    staging: {
+        wpMigrateSecretKey: '',
+        sslConfig: {
+            host: '',
+            port: 22,
+            passphrase: '',
+            username: '',
+            privateKey: fs.readFileSync('/Users/' + computerName + '/.ssh/id_rsa')
+        }
+    },
+    live: {
+        wpMigrateSecretKey: '',
+        sslConfig: {
+            host: '',
+            port: 22,
+            username: '',
+            privateKey: fs.readFileSync('/Users/' + computerName + '/.ssh/id_rsa')
+        }
+    }
 };
