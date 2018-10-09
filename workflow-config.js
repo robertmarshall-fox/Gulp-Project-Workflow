e7935faea51620827afaaaeea15dfaa57a848165
Rob Marshall <rob@fox.agency>
2018-10-09 15:46:32 +0100

    Untrack

--- workflow-config.js
+++ workflow-config.js
@@ -1,25 +1,30 @@
 var gulp         = require('gulp'); // Gulp of-course
 var fs           = require('fs');
 
-// module.exports = {
-//     themeFolder: '',
-//     local: {
-//
-//     },
-//     staging: {
-//         sslConfig: {
-//             host: '',
-//             port: 22,
-//             username: '',
-//             privateKey: fs.readFileSync('/Users/#####/.ssh/id_rsa')
-//         }
-//     },
-//     live: {
-//         sslConfig: {
-//             host: '',
-//             port: 22,
-//             username: '',
-//             privateKey: fs.readFileSync('/Users/#####/.ssh/id_rsa')
-//         }
-//     }
-// };
+const computerName = 'RobMarshall';
+
+module.exports = {
+    themeFolder: '',
+    local: {
+        computerName: computerName,
+    },
+    staging: {
+        wpMigrateSecretKey: '',
+        sslConfig: {
+            host: 'fh1.fox.agency',
+            port: 22,
+            passphrase: 'HB6aWUNe6w4&',
+            username: 'workflow',
+            privateKey: fs.readFileSync('/Users/' + computerName + '/.ssh/id_rsa')
+        }
+    },
+    live: {
+        wpMigrateSecretKey: '',
+        sslConfig: {
+            host: '',
+            port: 22,
+            username: '',
+            privateKey: fs.readFileSync('/Users/' + computerName + '/.ssh/id_rsa')
+        }
+    }
+};
