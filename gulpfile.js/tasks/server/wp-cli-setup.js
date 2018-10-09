d332ad2cde411a5abaa8c35c9e965bcd6f5c6eb0
Rob Marshall <rob@fox.agency>
2018-10-09 15:14:35 +0100

    Add promt for wp-cli download

--- gulpfile.js/tasks/server/wp-cli-setup.js
+++ gulpfile.js/tasks/server/wp-cli-setup.js
@@ -14,10 +14,22 @@ var config              = require('../../../workflow-config');
  * 1. Installs wp-cli on local
  */
 
-gulp.task('local-wp-cli-setup', () => {
-    console.log('Add WP-CLI to local environment'.red)
-    return gulp.src('*.js', {read: false})
-    .pipe(shell([
-        'cd ../../../ && curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
-    ]))
-});
+ gulp.task( 'local-wp-cli-setup', function() {
+     return gulp.src( './*')
+     .pipe(prompt.prompt({
+         type: 'confirm',
+         name: 'start',
+         message: 'Are you ready to download WP-CLI to local'.bgRed.white
+     }, function(res){
+         if(res.start){
+             console.log('Downloading WP-CLI to local environment'.red)
+             gulp.start('local-wp-cli-setup-download');
+         }
+     }));
+ });
+
+// Download
+gulp.task( 'local-wp-cli-setup-download', shell.task(
+    'cd ../../../ && ' +
+    'curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
+));
